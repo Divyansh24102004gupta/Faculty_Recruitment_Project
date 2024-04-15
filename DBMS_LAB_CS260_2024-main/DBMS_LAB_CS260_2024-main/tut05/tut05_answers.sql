91e81fde6269a077363d8b948f80e43954c334cd
Divyanshu Gupta <118093104+Divyansh24102004gupta@users.noreply.github.com>
2024-04-15 23:25:45 +0530

    Add files via upload

---
+++ DBMS_LAB_CS260_2024-main/DBMS_LAB_CS260_2024-main/tut05/tut05_answers.sql
@@ -0,0 +1,3 @@
+-- General Instructions
+-- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
+-- Comment in MYSQL 
