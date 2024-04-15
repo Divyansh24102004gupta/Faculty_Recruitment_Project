626584729c4429018b2256132814b8e0a6d9e897
Divyanshu Gupta <118093104+Divyansh24102004gupta@users.noreply.github.com>
2024-04-15 23:41:22 +0530

    Update tut07_answers.sql

--- DBMS_LAB_CS260_2024-main/DBMS_LAB_CS260_2024-main/tut07/tut07_answers.sql
+++ DBMS_LAB_CS260_2024-main/DBMS_LAB_CS260_2024-main/tut07/tut07_answers.sql
@@ -1,9 +1,6 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
--- General Instructions
--- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
--- Comment in MYSQL 
 
 -- 1. Create a procedure to calculate the average salary of employees in a given department.
 DELIMITER //
