2c406e31814bb03895aeb806885101b80a301c90
Divyanshu Gupta <118093104+Divyansh24102004gupta@users.noreply.github.com>
2024-04-15 23:40:13 +0530

    Update tut03_answers.sql

--- DBMS_LAB_CS260_2024-main/DBMS_LAB_CS260_2024-main/tut03/tut03_answers.sql
+++ DBMS_LAB_CS260_2024-main/DBMS_LAB_CS260_2024-main/tut03/tut03_answers.sql
@@ -1,9 +1,6 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
--- General Instructions
--- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
--- Comment in MYSQL 
 
 -- Assignments starts
 -- 1. Write a query to display the first name and last name of all students.
